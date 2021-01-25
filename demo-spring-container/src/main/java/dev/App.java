d1bf2cb85a004caf0a97bb7e3718d35aac305667
Rossi Oddet <rossi.oddet@dev-institut.fr>
2021-01-25 10:07:25 +0100

    demo spring config xml

--- demo-spring-container/src/main/java/dev/App.java
+++ demo-spring-container/src/main/java/dev/App.java
@@ -1,24 +1,23 @@
 package dev;
 
-import dev.metier.IMetier;
-import dev.metier.Metier1;
-import dev.persistance.IPersistance;
-import dev.persistance.Persistance1;
 import dev.presentation.Presentation1;
-
-import java.util.Scanner;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
 
-        IPersistance persitance = new Persistance1();
-        IMetier metier = new Metier1(persitance);
-        Scanner scanner = new Scanner(System.in);
-        Presentation1 presentation1 = new Presentation1(metier, scanner);
+//        IPersistance persitance = new Persistance1();
+//        IMetier metier = new Metier1(persitance);
+//        Scanner scanner = new Scanner(System.in);
+//        Presentation1 presentation1 = new Presentation1(metier, scanner);
 
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Presentation1 presentation1 = context.getBean(Presentation1.class);
+//        Presentation1 presentation2 = context.getBean(Presentation1.class);
+//        Presentation1 presentation3 = context.getBean(Presentation1.class);
+//        Presentation1 presentation4 = context.getBean(Presentation1.class);
         presentation1.print();
-
-
+        context.close();
     }
 }
